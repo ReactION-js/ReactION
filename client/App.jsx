3c528074e16c8da958528ca0b7ed54dc3176badd
Jinsung Park <js.lia.park@nyu.edu>
2019-03-13 19:42:42 -0400

    Refactored into React

---
+++ client/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				If you can see me, that means REACT works!
+			</div>
+		)
+	}
+}
+
+export default App;
