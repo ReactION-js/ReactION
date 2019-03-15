b0f8ca03a9f6f9c5c9b47578b8e2d85c7ce02896
Andy Tran <andytran@Andy-Trans-MacBook-Pro.local>
2019-03-15 16:17:06 -0400

    changes

--- out/extension.js
+++ out/extension.js
@@ -25,7 +25,7 @@ class ViewPanel {
         this._treePanel = treePanel;
         setInterval(() => {
             this._update();
-        }, 3000);
+        }, 1000);
         this._treePanel.onDidDispose(() => this.dispose(), null, this._disposables);
         this._treePanel.onDidChangeViewState(e => {
             if (this._treePanel.visible) {
@@ -91,7 +91,6 @@ class ViewPanel {
         }
     }
     async _update() {
-        console.log(page);
         let rawReactData = await page.scrape();
         this._treePanel.webview.html = this._getHtmlForWebview(rawReactData);
     }
