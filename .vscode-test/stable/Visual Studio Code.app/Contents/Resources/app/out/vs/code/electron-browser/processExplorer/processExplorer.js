052cc742af7c4c7c2e6d4c8b1289f482caf36569
Carson Chen <carson.cy.chen@gmail.com>
2019-03-20 10:26:30 -0400

    partial test

---
+++ .vscode-test/stable/Visual Studio Code.app/Contents/Resources/app/out/vs/code/electron-browser/processExplorer/processExplorer.js
@@ -0,0 +1,6 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+"use strict";const bootstrapWindow=require("../../../../bootstrap-window");bootstrapWindow.load(["vs/code/electron-browser/processExplorer/processExplorerMain"],function(o,r){o.startup(r.data)},{forceEnableDeveloperKeybindings:!0});
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/a3db5be9b5c6ba46bb7555ec5d60178ecc2eaae4/core/vs/code/electron-browser/processExplorer/processExplorer.js.map
